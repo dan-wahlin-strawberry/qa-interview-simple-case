23526a048cd2d6486a353bc13149977a78052982
dan-wahlin-strawberry <dan.wahlin@strawberry.se>
2024-07-05 13:02:53 +0200

    feat: add !

--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -3,7 +3,7 @@ import { existingUsers } from '../../test-setup/localstorage.setup'
 
 test.describe.configure({ mode: 'serial' })
 
-test.describe('login form tests', () => {
+test.describe('login form tests!', () => {
   test('logging in works with existing account', async ({ page }) => {
     await page.goto('localhost:8080/login')
 
